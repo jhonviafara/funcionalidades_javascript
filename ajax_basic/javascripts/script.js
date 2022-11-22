8ff789a92c677840ded0fc4bb781ec1bf6f4e80f
jhon alejandro viafara <jhon1144105@gmail.com>
2022-11-22 15:03:31 -0300

    actualizaciones

--- ajax_basic/javascripts/script.js
+++ ajax_basic/javascripts/script.js
@@ -1,12 +1,10 @@
 function obtenerDatos(){
 var peticion = new XMLHttpRequest();
-    peticion.open('GET', 'http://127.0.0.1:5500/datos.json') 
+    peticion.open('GET', 'datos.json') 
     
     peticion.onload = function () {
         var dato = JSON.parse(peticion.response)
-           document.getElementById("contenedor").innerHTML= "pais:"+dato.pais
-    }
-
-peticion.send();
+           document.getElementById("contenedor").innerHTML= "Nombre:"+dato.nombre}           
+peticion.send();                            
 };
 
